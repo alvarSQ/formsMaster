a6264f74620f27e579782ffa7ea9ba52a4411abe
Yaroslav Kh <yarik77@list.ru>
2024-03-02 09:09:38 +0500

    конец верстки

--- src/pages/question.vue
+++ src/pages/question.vue
@@ -1,21 +1,37 @@
 <template>
     <div class="form_wrapper">
         <div class="form_container">
-            <h2>Для чего вы используете Findmykids?</h2>
+            <div class="navbar-content">
+                <h2>Для чего вы используете Findmykids?</h2>
+                <p>1 из 6</p>
+            </div>
             <ul>
-                <li><label><input class="form_radio" type="radio" name="question" >Присматриваю за своим ребенком </label> </li>
-                <li>Присматриваю за своим ребенком</li>
-                <li>Присматриваю за своим ребенком</li>
+                <li class="form_lists"><label>
+                        <input class="form_radio" type="radio" name="question">
+                        Присматриваю за своим ребенком </label>
+                </li>
+                <li class="form_lists"><label><input class="form_radio" type="radio" name="question">Присматриваю за
+                        своим ребенком </label></li>
+                <li class="form_lists"><label class="navbar-content"><input class="form_radio" type="radio"
+                            name="question">
+                        <input type="text" placeholder="Свой вариант"></label></li>
             </ul>
             <button class="form_footer">
                 Ответить
             </button>
+            <div class="navbar-content">
+                <button class="form_footer">
+                    Назад
+                </button>
+                <button class="form_footer">
+                    Вперед
+                </button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-// import Questions from "@/components/varQuestions.vue";
 export default {
     components: {}
 }
